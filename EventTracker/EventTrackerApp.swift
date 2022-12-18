6a75071893a17f1b07fbd6911684c0f72f414be7
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2022-12-18 03:43:02 +0000

    Initial Commit

---
+++ EventTracker/EventTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventTrackerApp.swift
+//  EventTracker
+//
+//  Created by Don Bouncy on 18/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct EventTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
